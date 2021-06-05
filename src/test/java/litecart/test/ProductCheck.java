b0c7d7d445f55fde4c5c69092a610ab5f695828d
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-06-05 10:53:23 +0300

    Начато написание проверок для товаров на главной странице и странице товара

--- src/test/java/litecart/test/ProductCheck.java
+++ src/test/java/litecart/test/ProductCheck.java
@@ -1,5 +1,6 @@
 package litecart.test;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -22,5 +23,23 @@ public class ProductCheck extends TestBase {
                       "Цвет цены: " + colorPrice + "; текст стиль: " + stylePrice + ", размер шрифта: " + fontSizePrice +"\n" +
                       "Цвет акционной цены: " + colorPriceCampaign +"; текст жирность: " + stylePriceCampaign+ ", размер шрифта: " + fontSizeCampaign);
     firstDuck.click();
+    String nameOnProductForm = app.wd.findElement(By.cssSelector("h1")).getText();
+    String priceOnProductForm = app.wd.findElement(By.cssSelector("s.regular-price")).getText();
+    String priceCampaignOnProductForm = app.wd.findElement(By.cssSelector("strong.campaign-price")).getText();
+    String colorPriceOnProductForm = app.wd.findElement(By.cssSelector("s.regular-price")).getCssValue("color");
+    String colorPriceCampaignOnProductForm = app.wd.findElement(By.cssSelector("strong.campaign-price")).getCssValue("color");
+    String stylePriceOnProductForm =  app.wd.findElement(By.cssSelector("s.regular-price")).getCssValue("text-decoration-line");
+    String stylePriceCampaignOnProductForm =  app.wd.findElement(By.cssSelector("div.price-wrapper")).getCssValue("font-weight");
+    String fontSizePriceOnProductForm =  app.wd.findElement(By.cssSelector("s.regular-price")).getCssValue("font-size");
+    String fontSizeCampaignOnProductForm =  app.wd.findElement(By.cssSelector("div.price-wrapper")).getCssValue("font-size");
+
+    System.out.println("Имя: " + nameOnProductForm + "; Цена: " + priceOnProductForm + "; Акционная цена " + priceCampaignOnProductForm + "\n" +
+            "Цвет цены: " + colorPriceOnProductForm + "; текст стиль: " + stylePriceOnProductForm + ", размер шрифта: " + fontSizePriceOnProductForm +"\n" +
+            "Цвет акционной цены: " + colorPriceCampaignOnProductForm +"; текст жирность: " + stylePriceCampaignOnProductForm+ ", размер шрифта: " + fontSizeCampaignOnProductForm);
+
+    Assert.assertEquals(name,nameOnProductForm);
+    Assert.assertEquals(price,priceOnProductForm);
+    Assert.assertEquals(priceCampaign,priceCampaignOnProductForm);
+    Assert.assertEquals(stylePrice,stylePriceOnProductForm,"line-through");
   }
 }
