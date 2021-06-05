8e130ac017fcc675c4e2991fa55f3df2da1ca888
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-06-05 10:13:30 +0300

    В тест на проверку сортировки стран добавлена проверка: для тех стран, у которых количество зон отлично от нуля -- открыть страницу этой страны и там проверить, что зоны расположены в алфавитном порядке

--- src/test/java/litecart/test/CountriesSortCheck.java
+++ src/test/java/litecart/test/CountriesSortCheck.java
@@ -15,6 +15,7 @@ public class CountriesSortCheck extends TestBase {
   public void CountriesSortCheckTest(){
     app.loginAdmin();
     app.wd.findElement(By.xpath("//span[contains(.,'Countries')]")).click();
+    //а) проверить, что страны расположены в алфавитном порядке
     List<WebElement> countries = app.wd.findElements(By.cssSelector("tr.row td>a:not([title=Edit])"));
     ArrayList<String> names = new ArrayList<>();
     for (WebElement element : countries) {
@@ -24,12 +25,27 @@ public class CountriesSortCheck extends TestBase {
     notSortedNames.addAll(names);
     Collections.sort(names);
     Assert.assertEquals(notSortedNames, names);
-
+    //б) для тех стран, у которых количество зон отлично от нуля -- открыть страницу этой страны и там проверить, что зоны расположены в алфавитном порядке
     List<WebElement> countryRow = app.wd.findElements(By.cssSelector("tr.row"));
-    for (WebElement row : countryRow){
-      if(!row.findElement(By.cssSelector("td:nth-child(6)")).getText().equals("0")) {
-        row.findElement(By.cssSelector("td > a")).click();
+    for (int i=0; i < countryRow.size(); i++){
+      if(!countryRow.get(i).findElement(By.cssSelector("td:nth-child(6)")).getText().equals("0")) {
+        countryRow.get(i).findElement(By.cssSelector("td > a")).click();
         List<WebElement> zones = app.wd.findElements(By.cssSelector("td:nth-child(3)>input[name^='zones']"));
+        ArrayList<String> namesZones = new ArrayList<>();
+        for (WebElement element : zones) {
+          namesZones.add(element.getAttribute("value"));
+        }
+        ArrayList<String> notSortedZoneNames = new ArrayList<>();
+        notSortedZoneNames.addAll(namesZones);
+        Collections.sort(namesZones);
+
+        Assert.assertEquals(notSortedZoneNames, namesZones);
+
+        zones.clear();
+        namesZones.clear();
+        notSortedZoneNames.clear();
+        app.wd.navigate().back();
+        countryRow = app.wd.findElements(By.cssSelector("tr.row"));
       }
     }
   }
