ccc97a1fdd0ddc08fc6f5aa132b9c4f164030db2
GorunovaMargarita <margo.aleksandrovna@ya.ru>
2021-06-11 09:14:44 +0300

    Завершена разработка теста на добавление и удаление товаров из корзины

--- src/test/java/litecart/test/DeleteProductsFromBasket.java
+++ src/test/java/litecart/test/DeleteProductsFromBasket.java
@@ -1,9 +1,11 @@
 package litecart.test;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 
 
 public class DeleteProductsFromBasket extends TestBase {
@@ -16,7 +18,10 @@ public class DeleteProductsFromBasket extends TestBase {
             WebElement firstDuck = app.wd.findElements(By.cssSelector("#box-most-popular a.link")).get(0);
             firstDuck.click();
             WebElement quantity = app.wd.findElement(By.cssSelector("span.quantity"));
-
+            if(!app.isElementNotPresent(By.cssSelector("select"))){
+                Select select = new Select(app.wd.findElement(By.cssSelector("select")));
+                select.selectByIndex(1);
+            }
             duckCountInBasket = Integer.parseInt(quantity.getText());
             app.wd.findElement(By.cssSelector("button[name=add_cart_product")).click();
             app.wait.until(ExpectedConditions.attributeToBe(quantity,"textContent",String.valueOf(duckCountInBasket+1)));
@@ -24,11 +29,13 @@ public class DeleteProductsFromBasket extends TestBase {
             app.wd.findElement(By.cssSelector("i.fa.fa-home")).click();
         } while (duckCountInBasket < nedeedDuckCount);
         app.wd.findElement(By.cssSelector("a.link[href$='checkout']")).click();
-        for (int i = 0; i < nedeedDuckCount; i++)
+        int countInBasket = app.wd.findElements(By.cssSelector("a.image-wrapper.shadow")).size();
+        for (int i = 0; i < countInBasket; i++)
         {
             WebElement table = app.wd.findElement(By.cssSelector("table.dataTable.rounded-corners"));
             app.wd.findElement(By.cssSelector("button[value='Remove']")).click();
             app.wait.until(ExpectedConditions.stalenessOf(table));
         }
+        Assert.assertEquals(app.wd.findElement(By.cssSelector("p em")).getText(),"There are no items in your cart.");
     }
 }
